2870d5adf8c80c9e826554cdcb268735a04ae291
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-07-31 10:54:33 +0200

    Routing Metadata

--- course/app/products/[productId]/page.tsx
+++ course/app/products/[productId]/page.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { Metadata } from "next";
+import { time } from "console";
+import { resolve } from "path";
 
-const ProducDetails = ({
-  params,
-}: {
+type Props = {
   params: {
     productId: string;
   };
-}) => {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Iphone ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+const ProducDetails = ({ params }: Props) => {
   return (
     <div>
       <h1>Product {params.productId} Details.</h1>
