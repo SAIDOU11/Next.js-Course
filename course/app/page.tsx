14250a8743712ecb32cb6c5fe940610dd3dd616f
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-08-01 09:58:13 +0200

    link & active links

--- course/app/page.tsx
+++ course/app/page.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+import Link from "next/link";
+
 const Home = () => {
   return (
     <div>
       <h1>Welcome Home 🏠</h1>
+      <Link href="/blog" className="block m-4">
+        Blog
+      </Link>
+
+      <Link href="/products" className="block m-4">
+        Products
+      </Link>
     </div>
   );
 };
